f04c41dc21f336cf93787fceecd5d96632403044
BruceMu <morningmcq@163.com>
2014-09-12 17:39:24 +0800

    测试FFTW代码未调试

--- src/SFFT_2D_OFDM/TestFFTW.cpp
+++ src/SFFT_2D_OFDM/TestFFTW.cpp
@@ -1,14 +1,28 @@
 #include <fftw3.h>
 #include <iostream>
+#include <fstream>
 void main()
 {
+    int N = 0;
     fftw_complex * in, * out;
     fftw_plan p;
+    char * src = new char[N];
 
-    in = (fftw_complex*)fftw_molloc(sizeof(fftw_complex) * N);
+    ifstream fin("d:\\test_data.txt",ios::nocreate|ios::binary);
+    char c;
+    int i = 0;
+    while((c = fin.get()!=EOF){
+        src[i] = c;
+        i++;
+    }
+//  in = (fftw_complex*)fftw_molloc(sizeof(fftw_complex) * N);
+    in = (double *)fftw_molloc(sizeof(double)*N);
     out = (fftw_complex*)fftw_molloc(sizeof(fftw_complex) * N);
-
-    p = fftw_plan_dft_1d(N,in,out,FFTW_FORWORD,FFTW_ESTIMATE);
+    for(int i = 0;i< N;i++){
+        in[i] = (double)src[i];
+    }
+//  p = fftw_plan_dft_1d(N,in,out,FFTW_FORWORD,FFTW_ESTIMATE);
+    p = fftw_plan_dft_r2c_1d(N,in,out,FFTW_FORWORD,FFTW_ESTIMATE);
     fftw_execute(p);
 
 
